e88a5e45cce4236229ff79cb6b09abc13f1729f8
Darren Schnare <schnare.d@gmail.com>
2015-12-21 15:58:04 -0500

    Only null properties are injected
    Only properties that have been set to null will be considered
    when injecting dependencies.

---
+++ packages/ioc-container/HISTORY.md
@@ -0,0 +1,8 @@
+# 0.0.2
+
+**Dec 21, 2015**
+
+- Properties that are prefixed with `_` are skipped when dependencies are being
+  injected.
+
+- Only properties that are set to `null` will be injected as dependencies.
--- packages/ioc-container/package.js
+++ packages/ioc-container/package.js
@@ -1,11 +1,11 @@
 /*global Package*/
 Package.describe({
   name: 'dschnare:ioc-container',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'A simple IOC container for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/dschnare/meteor-ioc-container',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
