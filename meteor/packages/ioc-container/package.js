1314c27297464c5be91fee7e5513a5ec5874e8e8
Darren Schnare <schnare.d@gmail.com>
2016-07-28 09:05:22 -0400

    Update meteor package

--- meteor/packages/ioc-container/package.js
+++ meteor/packages/ioc-container/package.js
@@ -12,7 +12,7 @@ Package.describe({
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.2.1')
+  api.versionsFrom('1.3')
   api.use('ecmascript')
   api.mainModule('src/IocContainer.js')
 })
